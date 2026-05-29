d39a024d3fb19d26a151412311fd919721884ff5
FaithL123 <waytochrist200820@gmail.com>
2026-05-29 13:06:12 +0800

    Update database configuration variables in config.py

--- app/config.py
+++ app/config.py
@@ -8,5 +8,6 @@ class Config:
     STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
     STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
     DB_SERVER = os.getenv('DB_SERVER')
-    DB_USERNAME = os.getenv('DB_USERNAME')
+    DB_NAME = os.getenv('DB_NAME')
+    DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
