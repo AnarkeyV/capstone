d55e24aac03fa1b1772a44d5a66ffa07c471852f
Khairul Rizal <kuhei.raiza@gmail.com>
2026-06-04 13:47:13 +0800

    Use supported AKS node VM size for staging

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -61,7 +61,7 @@ variable "aks_node_count" {
 variable "aks_node_vm_size" {
   description = "VM size for AKS node pool."
   type        = string
-  default     = "Standard_B2s"
+  default     = "Standard_B2s_v2"
 }
 
 variable "log_analytics_workspace_name" {
